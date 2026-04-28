b8527babcb3a1c615f85c46d239b74b371140b10
fr.garaym@duocuc.cl <fr.garaym@duocuc.cl>
2026-04-27 22:11:32 -0400

    se agrega microservicios de categoria y rol

--- Microservicio-Categoria/src/main/java/com/categoria/Microservicio_Categoria/MicroservicioCategoriaApplication.java
+++ Microservicio-Categoria/src/main/java/com/categoria/Microservicio_Categoria/MicroservicioCategoriaApplication.java
@@ -1,13 +1,13 @@
-package com.prenda.Microservicio_Prenda;
+package com.categoria.Microservicio_Categoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicioPrendaApplication {
+public class MicroservicioCategoriaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicioPrendaApplication.class, args);
+		SpringApplication.run(MicroservicioCategoriaApplication.class, args);
 	}
 
 }
--- Microservicio-Categoria/src/test/java/com/categoria/Microservicio_Categoria/MicroservicioCategoriaApplicationTests.java
+++ Microservicio-Categoria/src/test/java/com/categoria/Microservicio_Categoria/MicroservicioCategoriaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cliente.Microservicio_Cliente;
+package com.categoria.Microservicio_Categoria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicioClienteApplicationTests {
+class MicroservicioCategoriaApplicationTests {
 
 	@Test
 	void contextLoads() {
